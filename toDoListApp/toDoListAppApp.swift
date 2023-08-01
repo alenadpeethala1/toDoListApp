fe506da637d8fba62e521eb5801ed4b6ef141b92
Alena Peethala <alenadashi@icloud.com>
2023-08-01 10:56:05 -0700

    Initial Commit

---
+++ toDoListApp/toDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListAppApp.swift
+//  toDoListApp
+//
+//  Created by Alena Peethala on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
